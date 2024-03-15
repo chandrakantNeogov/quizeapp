17a04bd9ee23502a4f75d1ab1c400840d35cf0b3
Chandrakant Kondke <151041656+chandrakantNeogov@users.noreply.github.com>
2024-03-15 17:51:41 +0530

    Initial Commit

---
+++ quizeapp/quizeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizeappApp.swift
+//  quizeapp
+//
+//  Created by Chandrakant  Kondke on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct quizeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
